cd63aab1712add3b085aec256df49bfc6837ac82
workvivek <workvivek603@gmail.com>
2024-11-25 13:15:43 +0530

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
 import React from 'react'
 import Home from './Pages/Home'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Menu from './Pages/Menu'
 import Gallery from './Pages/Gallery'
 import AboutUs from './Pages/AboutUs'
 import ContactUs from './Pages/ContactUs'
 import ViewCard from './Pages/ViewCard'
-// import AddCard from './Pages/AddCard'
 import Chekout from './Pages/Chekout'
 import Login from './Pages/Login'
-import Card1 from './AddCard/card1'
+import Card1 from './AddCard/Card1'
 import Card2 from './AddCard/Card2'
-
 
 function App() {
   return (
-    <div className='relative font-montserrat' >
-      
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/Menu" element={<Menu/>}/>
-      <Route path="/Gallery" element={<Gallery/>}/>
-      <Route path="/AboutUs" element={<AboutUs/>}/>
-      <Route path='/ContactUs' element={<ContactUs/>}/>
-      <Route path="/ViewCard" element={<ViewCard/>}/>
-      {/* <Route path='/AddCard' element={<AddCard/>}/> */}
-      <Route path='/Chekout' element={<Chekout/>}/>
-      <Route path='/Login' element={<Login/>}/>
-      <Route path='/Card1' element={<Card1/>}/>
-      <Route path='/Card2' element={<Card2/>}/>
-    </Routes>
+    <div className='relative font-montserrat'>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Menu" element={<Menu/>} />
+        <Route path="/Gallery" element={<Gallery/>} />
+        <Route path="/AboutUs" element={<AboutUs/>} />
+        <Route path='/ContactUs' element={<ContactUs/>} />
+        <Route path="/ViewCard" element={<ViewCard/>} />
+        <Route path='/Chekout' element={<Chekout/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/Card1' element={<Card1/>} />
+        <Route path='/Card2' element={<Card2/>} />
+      </Routes>
     </div>
   )
-}      
+}
 
 export default App
