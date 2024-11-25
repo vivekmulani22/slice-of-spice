38f2692b1df9e213458ef9080189a39188198c21
workvivek <workvivek603@gmail.com>
2024-11-25 13:39:29 +0530

    5

--- src/Home/Sawipar.jsx
+++ src/Home/Sawipar.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import {Autoplay, Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { assets } from "../assets/assets";
 
-
 import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-// import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Sawipar = () => {
   return (
-    <>
-     <div class="swiper-pagination"></div>
-      <div className="w-full flex items-center text-center justify-center ">
+    <div className="w-full flex items-center text-center justify-center">
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -28,73 +24,71 @@ const Sawipar = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-          <SwiperSlide className="flex flex-col text-center items-center justify-center px-[11vw] py-8  ">
-            <h1 className="text-xl font-bold">A TASTE YOU'LL REMEMBER</h1>
-            <br />
-            <img src={assets.Group2} alt="" className=" w-[4vw]" />
-            <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
-              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
-              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
-            </p>
-            <br />
-            <img
-              src={assets.Ellipse}
-              alt=""
-              className="w-[4vw] items-center  "
-            />
-            <br />
-            <h4>jAMES DE FRANCO</h4>
-          </SwiperSlide>
-          <SwiperSlide className="flex flex-col text-center items-center justify-center px-[11vw] py-8  ">
-            <h1 className="text-xl font-bold">A TASTE YOU'LL REMEMBER</h1>
-            <br />
-            <img src={assets.Group2} alt="" className=" w-[4vw]" />
-            <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
-              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
-              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
-            </p>
-            <br />
-            <img
-              src={assets.Ellipse}
-              alt=""
-              className="w-[4vw] items-center  "
-            />
-            <br />
-            <h4>jAMES DE FRANCO</h4>
-          </SwiperSlide>
-          <SwiperSlide className="flex flex-col text-center items-center justify-center px-[11vw] py-8  ">
-            <h1 className="text-xl font-bold">A TASTE YOU'LL REMEMBER</h1>
-            <br />
-            <img src={assets.Group2} alt="" className=" w-[4vw]" />
-            <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
-              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
-              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
-            </p>
-            <br />
-            <img
-              src={assets.Ellipse}
-              alt=""
-              className="w-[4vw] items-center  "
-            />
-            <br />
-            <h4>jAMES DE FRANCO</h4>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-      
-    </>
+        <SwiperSlide className="flex flex-col text-center items-center justify-center px-[11vw] py-8">
+          <h1 className="text-xl font-bold">A TASTE YOU'LL REMEMBER</h1>
+          <br />
+          <img src={assets.Group2} alt="Decorative icon" className="w-[4vw]" />
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+            nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+            tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+            feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+          </p>
+          <br />
+          <img
+            src={assets.Ellipse}
+            alt="Profile"
+            className="w-[4vw]"
+          />
+          <br />
+          <h4>JAMES DE FRANCO</h4>
+        </SwiperSlide>
+        <SwiperSlide className="flex flex-col text-center items-center justify-center px-[11vw] py-8">
+          <h1 className="text-xl font-bold">A TASTE YOU'LL REMEMBER</h1>
+          <br />
+          <img src={assets.Group2} alt="Decorative icon" className="w-[4vw]" />
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+            nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+            tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+            feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+          </p>
+          <br />
+          <img
+            src={assets.Ellipse}
+            alt="Profile"
+            className="w-[4vw]"
+          />
+          <br />
+          <h4>JAMES DE FRANCO</h4>
+        </SwiperSlide>
+        <SwiperSlide className="flex flex-col text-center items-center justify-center px-[11vw] py-8">
+          <h1 className="text-xl font-bold">A TASTE YOU'LL REMEMBER</h1>
+          <br />
+          <img src={assets.Group2} alt="Decorative icon" className="w-[4vw]" />
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+            nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+            tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+            feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+          </p>
+          <br />
+          <img
+            src={assets.Ellipse}
+            alt="Profile"
+            className="w-[4vw]"
+          />
+          <br />
+          <h4>JAMES DE FRANCO</h4>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
